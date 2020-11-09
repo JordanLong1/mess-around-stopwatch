d9d48494cb9de2a2ef4b08e1fb4253f9eec01f35
Jordan <jordanleelong5@gmail.com>
2020-11-08 23:09:55 -0800

    added className

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import StopWatch from './components/StopWatch'
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='container-holder'>
         <StopWatch />
       </div>
     )
