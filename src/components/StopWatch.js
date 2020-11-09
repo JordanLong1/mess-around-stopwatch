7451508167ad19e6791b5f8227296815de41c172
Jordan <jordanleelong5@gmail.com>
2020-11-08 23:16:19 -0800

    added header and p tag

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -22,6 +22,8 @@ class StopWatch extends React.Component {
     render() {
         return <div className='watch-container'>
             <div className='watch-div'>
+                <h1>StopWatch</h1>
+                <p>Click on start to start your clock, click stop to stop the timer and reset it</p>
                 {this.state.start}
                 <button onClick={this.intervalHelper} className='btn'>Start your timer</button>
                 <button onClick={this.clearTimer} className='btn'>Stop / Reset</button>
